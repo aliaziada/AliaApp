d48eb049f2336a876c2811faa9f0a86ab17cbae3
Alia Ziada <aliaziada@Aliaas-MacBook-Pro.local>
2015-12-31 18:31:24 +0200

    AliaShowCase

---
+++ AliaShowCase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AliaShowCase
+//
+//  Created by Alia Ziada on 12/31/15.
+//  Copyright © 2015 Ntime. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
